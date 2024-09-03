a0d0070da7ddb3e1318e2b0232a9968bcae5026e
Shomail Bin Tahir <sbt@scaleup.finance>
2024-09-03 21:13:43 +0200

    add max count logic

--- src/components/Header/CommentForm.tsx
+++ src/components/Header/CommentForm.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
+const MAX_TEXT_COUNT = 150;
+
 export const CommentForm = () => {
+  const [text, setText] = useState('');
+
   return (
-    <form className="form">
-      <textarea id="feedback-textarea" placeholder="feedback" spellCheck={false} />
+    <form className={`form ${text.length > MAX_TEXT_COUNT ? 'form--invalid' : ''}`}>
+      <textarea
+        id="feedback-textarea"
+        placeholder="feedback"
+        spellCheck={false}
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <label htmlFor="feedback-textarea">Enter your feedback here!</label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{MAX_TEXT_COUNT - text.length}</p>
         <button>
           <span>Submit</span>
         </button>
