c8807578a526f4cd816f43ae03dce0b323cc05ad
Shomail Bin Tahir <sbt@scaleup.finance>
2024-09-03 17:28:31 +0200

    add placeholder comment

--- src/components/CommentsList/CommentsList.tsx
+++ src/components/CommentsList/CommentsList.tsx
@@ -1,3 +1,25 @@
+import { TriangleUpIcon } from '@radix-ui/react-icons';
+
 export const CommentsList = () => {
-  return <ol className="feedback-list">CommentsList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>539</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>Company Name</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda blanditiis, dicta facere quisquam
+            laborum sint.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 };
