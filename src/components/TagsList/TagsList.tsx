74c61b82e5b9d158a4792a7a11d9392b4a588ed5
Shomail Bin Tahir <sbt@scaleup.finance>
2024-09-12 20:00:57 +0200

    add tags list

--- src/components/TagsList/TagsList.tsx
+++ src/components/TagsList/TagsList.tsx
@@ -1,12 +1,27 @@
+import { useComments } from '../../api/comments';
+
 export const TagsList = () => {
+  const { data, error } = useComments();
+
+  if (error) {
+    return <div className="error-message">{error.message}</div>;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const tags = data
+    .map((comment) => ({ id: comment.id, company: comment.company }))
+    .filter((tag, index, self) => self.findIndex((t) => t.company === tag.company) === index);
+
   return (
     <ul className="hashtags">
-      <li>
-        <button>Tag 1</button>
-      </li>
-      <li>
-        <button>Tag 2</button>
-      </li>
+      {tags.map((tag) => (
+        <li key={tag.id}>
+          <button>{tag.company}</button>
+        </li>
+      ))}
     </ul>
   );
 };
